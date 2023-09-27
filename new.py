9a4710c4bf60eafa67fecab14acb7d2032c9fc67
Akshay Kalyanrao Erande <akerande@outlook.com>
2023-09-27 18:06:10 +0530

    cleaning of file

--- new.py
+++ new.py
@@ -1,22 +1,6 @@
-# import os
-# if os.path.exists("C:\\Users\\HP G5\\OneDrive\\Desktop\\test.txt"):
-#     print("path exist")
-#     os.remove("C:\\Users\\HP G5\\OneDrive\\Desktop\\test.txt")
-#     print("file removed")
-# else:
-#     print("path not exist")
-
-#
-# import os
-# os.rmdir("C:\\Users\\HP G5\\OneDrive\\Desktop\\aaa")
-
-
 import pandas as pd
 df = pd.read_csv ('file_name.csv')
 print(df)
 new_df = pd.read_csv('file_name.csv', usecols= ['column_name1','column_name2'])
 print(new_df)
-
-
-#changes by new akshay
 
