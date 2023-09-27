efdfc9e13ec9fd889e926f132bcb49572e12b70c
monikaerande <monika.bamnodkar@outlook.com>
2023-09-27 16:52:02 +0530

    my first commit

---
+++ new.py
@@ -0,0 +1,12 @@
+# import os
+# if os.path.exists("C:\\Users\\HP G5\\OneDrive\\Desktop\\test.txt"):
+#     print("path exist")
+#     os.remove("C:\\Users\\HP G5\\OneDrive\\Desktop\\test.txt")
+#     print("file removed")
+# else:
+#     print("path not exist")
+
+#
+# import os
+# os.rmdir("C:\\Users\\HP G5\\OneDrive\\Desktop\\aaa")
+
